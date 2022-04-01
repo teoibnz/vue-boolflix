6c1c6df254ab6a28b1392b06751485269547994d
Matteo Lopez <matteolopezbb@gmail.com>
2022-04-01 12:43:58 +0200

    installazione axios e bootstrap

--- src/App.vue
+++ src/App.vue
@@ -20,12 +20,9 @@ export default {
 </script>
 
 <style lang="scss">
+@import './assets/styles/style.scss';
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <header>
+        <select name="" id=""></select>
+        <a href="" class="btn"></a>
+    </header>
+</template>
+
+<script>
+export default {
+    name : 'IndexHeader',
+}
+</script>
+
+<style scoped lang='scss'>
+
+</style>
