5099fb6def36f7120ab59bdbd35a12e266a3744e
Matteo Lopez <matteolopezbb@gmail.com>
2022-04-06 12:38:42 +0200

    modifiche scss effetto hover

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,13 +2,14 @@
     <main>
         <h3 class="text-white">ORIGINALI NETFLIX</h3>
         <div class="container-fluid d-flex">
-        <div class="row my-film-container position-relative" v-for='(film, index) in filmList' :key="index" >
+        <div class="row my-film-container position-relative overflowy-scroll" v-for='(film, index) in filmList' :key="index" >
             <span class="col-12">
                 <img class="mt-4" :src="filmPoster(film.poster_path)" alt="film poster">
             </span>
-            <div class="my-film-info">
+            <div class="my-film-info p-5">
                 <p  >{{film.title || film.name}}</p   >
                 <p  >{{film.original_title || film.original_name}}</p >
+                <p> {{film.overview}} </p>
                 <span>
                     <country-flag :country='flags(film.original_language)' size='big'/>
                 </span>
@@ -58,25 +59,33 @@ export default {
 </script>
 
 <style scoped lang='scss'>
-div.my-film-container{
-    img{
-        height: 479px;
-    }
 
-    .my-film-info{
-        position: absolute;
-        top: 40px;
-        height: 80%;
-        color: white;
-        text-align: center;
-        display: none;
-    }
+
+main{
+    overflow-x: scroll;
+    div.my-film-container{
+        img{
+            height: 479px;
+        }
+
+        .my-film-info{
+            position: absolute;
+            top: 40px;
+            height: 80%;
+            color: white;
+            text-align: center;
+            display: none;
+        }
+    } 
 }
 div.my-film-container:hover .my-film-info{
     display: inline-block;
 }
 div.my-film-container:hover img{
     opacity: 0.2;
+    filter: blur(3px);
+    transform: scale(1.1);
+    z-index: 2;
 }
 
 </style>
